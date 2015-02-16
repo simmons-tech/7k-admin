ce06c185bb0359154f66c2fe7cddd9cb182972c4
Lars Johnson <larsj@mit.edu>
2015-02-15 21:20:03 -0500

    Initial pics app

--- admin7k/urls.py
+++ admin7k/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
+from pics import views as pics_views
+
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'admin7k.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^pics/', include('pics.urls', namespace='rooming')),
     url(r'^admin/', include(admin.site.urls)),
 )
--- pics/views.py
+++ pics/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
+def index(request):
+    context = {}
+    return render(request, 'pics/index.html', context)
+
 # Create your views here.
