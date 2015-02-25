19fab2b8fd42679df4b31d9c1cfd9510f5a3e72e
Amol Bhave <ammubhave@gmail.com>
2015-02-24 19:55:42 -0500

    make it deploybable

--- admin7k/wsgi.py
+++ admin7k/wsgi.py
@@ -7,6 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
 """
 
+import sys
+sys.path.append(os.path.dirname(os.path.dirname(__file__)))
+
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "admin7k.settings")
 
